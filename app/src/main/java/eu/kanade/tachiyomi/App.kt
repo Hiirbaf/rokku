8ac5d4e862976daab28e87a0a1e59660c8ad54a0
Thiago Rocha <thiago8rocha@gmail.com>
2026-07-30 12:27:14 -0300

    fix: disable hardware bitmaps on Android 8.0/8.1 to prevent reader crash
    Coil defaults to allowHardware=true; explicitly disabling it below API
    28 avoids the documented Android 8.0/8.1 OS crash when a hardware
    bitmap is drawn to a software canvas (coil-kt/coil#159). This doesn't
    reintroduce the unrelated Samsung GPU driver crash (coil-kt/coil#1038)
    that allowHardware was previously commented out for, since that
    default was already true either way on API 28+.
    
    Ported from Jays2Kings/tachiyomiJ2K@046551af5.

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -32,7 +32,7 @@ import coil3.PlatformContext
 import coil3.SingletonImageLoader
 import coil3.memory.MemoryCache
 import coil3.network.okhttp.OkHttpNetworkFetcherFactory
-// import coil3.request.allowHardware
+import coil3.request.allowHardware
 import coil3.request.allowRgb565
 import coil3.request.crossfade
 import coil3.util.DebugLogger
@@ -313,7 +313,9 @@ open class App : Application(), DefaultLifecycleObserver, SingletonImageLoader.F
 
             crossfade(true)
             allowRgb565(this@App.getSystemService<ActivityManager>()!!.isLowRamDevice)
-            // allowHardware(true)
+            // Android 8.0/8.1 can crash when a hardware bitmap is drawn to a software canvas.
+            // https://github.com/coil-kt/coil/issues/159
+            allowHardware(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
             if (networkPreferences.verboseLogging().get()) {
                 logger(DebugLogger())
             }
