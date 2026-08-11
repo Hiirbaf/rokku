933266be6c0836f3fb88fed64d71db5b71e8951d
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-08-10 23:43:46 -0300

    Refactor OAuth login handling to use lifecycleScope

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/track/MangaBakaLoginActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/track/MangaBakaLoginActivity.kt
@@ -1,13 +1,18 @@
 package eu.kanade.tachiyomi.ui.setting.track
 
 import android.net.Uri
+import androidx.lifecycle.lifecycleScope
+import eu.kanade.tachiyomi.util.system.launchIO
 
 class MangaBakaLoginActivity : BaseOAuthLoginActivity() {
     override fun handleResult(data: Uri?) {
         val state = data?.getQueryParameter("state")
         val code = data?.getQueryParameter("code")
         if (code != null && state != null && trackManager.mangaBaka.verifyOAuthState(state)) {
-            login { trackManager.mangaBaka.login(code) }
+            lifecycleScope.launchIO {
+                trackManager.mangaBaka.login(code)
+                returnToSettings()
+            }
         } else {
             trackManager.mangaBaka.logout()
             returnToSettings()
