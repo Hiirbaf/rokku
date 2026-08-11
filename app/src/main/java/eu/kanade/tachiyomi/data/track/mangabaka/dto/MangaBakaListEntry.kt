c8caaa8646a9e337a38156a8117ba7c0ea189532
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-08-11 00:07:07 -0300

    Remove default value for isPrivate field
    Removed default value for isPrivate in MangaBakaListEntry.

--- app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/dto/MangaBakaListEntry.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/dto/MangaBakaListEntry.kt
@@ -19,8 +19,6 @@ data class MangaBakaListEntry(
     @SerialName("progress_chapter")
     val progressChapter: Double?,
     val rating: Int?,
-    @SerialName("is_private")
-    val isPrivate: Boolean = false,
 ) {
     fun getStatus(): Int =
         when (state) {
