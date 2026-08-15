c2d1e375509a2f4d74eab436030e96be82938563
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-08-15 10:50:44 -0300

    Change coverThemeStyle to use integer ordinal

--- app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -427,7 +427,7 @@ class PreferencesHelper(val context: Context, val preferenceStore: PreferenceSto
     fun showNsfwSources() = preferenceStore.getBoolean(Keys.showNsfwSource, true)
 
     fun themeMangaDetails() = preferenceStore.getBoolean(Keys.themeMangaDetails, true)
-    fun coverThemeStyle() = preferenceStore.getEnum(Keys.coverThemeStyle, PaletteStyle.TonalSpot)
+    fun coverThemeStyle() = preferenceStore.getInt(Keys.coverThemeStyle, PaletteStyle.TonalSpot.ordinal)
 
     fun renderDescriptionImages() = preferenceStore.getBoolean(Keys.renderDescriptionImages, true)
 
