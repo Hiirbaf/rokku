d62f61a6b869279019a6c7e7cec84197244cc6cb
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-08-18 17:18:15 -0300

    Remove coverThemeOptions and labelRes function
    Removed coverThemeOptions list and labelRes function from SettingsAppearanceController.

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsAppearanceController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsAppearanceController.kt
@@ -23,6 +23,8 @@ import eu.kanade.tachiyomi.ui.setting.switchPreference
 import eu.kanade.tachiyomi.ui.setting.themePreference
 import eu.kanade.tachiyomi.util.system.SideNavMode
 import eu.kanade.tachiyomi.util.system.appDelegateNightMode
+import eu.kanade.tachiyomi.util.system.coverThemeOptions
+import eu.kanade.tachiyomi.util.system.labelRes
 import eu.kanade.tachiyomi.util.system.dpToPx
 import eu.kanade.tachiyomi.util.system.getPrefTheme
 import eu.kanade.tachiyomi.util.system.isInNightMode
@@ -42,21 +44,6 @@ class SettingsAppearanceController : SettingsLegacyController() {
     var lastThemeXLight: Int? = null
     var lastThemeXDark: Int? = null
     var themePreference: ThemePreference? = null
-
-    val coverThemeOptions: List<PaletteStyle?> = listOf(null) + PaletteStyle.entries
-
-    fun PaletteStyle?.labelRes(): StringResource = when (this) {
-        null -> MR.strings.legacy_theme
-        PaletteStyle.TonalSpot -> MR.strings.palette_style_tonal_spot
-        PaletteStyle.Neutral -> MR.strings.palette_style_neutral
-        PaletteStyle.Vibrant -> MR.strings.palette_style_vibrant
-        PaletteStyle.Expressive -> MR.strings.palette_style_expressive
-        PaletteStyle.Rainbow -> MR.strings.palette_style_rainbow
-        PaletteStyle.FruitSalad -> MR.strings.palette_style_fruit_salad
-        PaletteStyle.Monochrome -> MR.strings.palette_style_monochrome
-        PaletteStyle.Fidelity -> MR.strings.palette_style_fidelity
-        PaletteStyle.Content -> MR.strings.palette_style_content
-    }
 
     @SuppressLint("NotifyDataSetChanged")
     override fun setupPreferenceScreen(screen: PreferenceScreen) = screen.apply {
