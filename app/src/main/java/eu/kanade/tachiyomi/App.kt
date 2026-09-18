ea3eeb678596cad17f2a8f8d421a7ade661f061b
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-09-18 08:36:33 -0300

    Add Discord RPC service lifecycle methods

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -46,6 +46,7 @@ import eu.kanade.tachiyomi.data.coil.MangaCoverFetcher
 import eu.kanade.tachiyomi.data.coil.MangaCoverKeyer
 import eu.kanade.tachiyomi.data.coil.MangaKeyer
 import eu.kanade.tachiyomi.data.coil.TachiyomiImageDecoder
+import eu.kanade.tachiyomi.data.connections.discord.DiscordRPCService
 import eu.kanade.tachiyomi.data.download.DownloadJob
 import eu.kanade.tachiyomi.data.notification.Notifications
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
@@ -214,6 +215,14 @@ open class App : Application(), DefaultLifecycleObserver, SingletonImageLoader.F
                 preference.set(BuildConfig.VERSION_CODE)
             },
         )
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        DiscordRPCService.start(applicationContext)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        DiscordRPCService.stop(applicationContext)
     }
 
     override fun onPause(owner: LifecycleOwner) {
