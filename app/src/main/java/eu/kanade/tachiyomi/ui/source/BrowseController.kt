3b93f52c748705151813b78eae38087b4ffa4f73
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-09-22 14:05:18 -0300

    Prevent saving search history if suppressed

--- app/src/main/java/eu/kanade/tachiyomi/ui/source/BrowseController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/source/BrowseController.kt
@@ -713,7 +713,9 @@ class BrowseController :
     }
 
     private fun performGlobalSearch(query: String) {
-        preferences.addToSearchHistory(query)
+        if (!searchHistory.consumeSuppressSave()) {
+            preferences.addToSearchHistory(query)
+        }
         searchHistory.setVisible(false)
         router.pushController(GlobalSearchController(query).withFadeTransaction())
     }
