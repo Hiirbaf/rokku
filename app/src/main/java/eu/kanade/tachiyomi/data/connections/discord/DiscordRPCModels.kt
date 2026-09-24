e34e8af358412c140301c0fd78a4c2c52545e698
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2026-09-24 14:47:41 -0300

    Uncomment WEBVIEW entry in DiscordRPCModels

--- app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/DiscordRPCModels.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/DiscordRPCModels.kt
@@ -32,14 +32,14 @@ enum class DiscordScreen(
     HISTORY(R.string.app_name, R.string.scrolling, R.string.recents, HISTORY_IMAGE_URL),
     BROWSE(R.string.app_name, R.string.browsing, R.string.browse, BROWSE_IMAGE_URL),
     MORE(R.string.app_name, R.string.messing, R.string.settings, moreImageUrl),
-    // WEBVIEW(R.string.app_name, R.string.browsing, R.string.action_web_view, webviewImageUrl),
+    WEBVIEW(R.string.app_name, R.string.browsing, R.string.action_web_view, webviewImageUrl),
     MANGA(R.string.app_name, R.string.comic, R.string.reading, mangaImageUrl),
 }
 
 private const val CDN = "https://cdn.discordapp.com/"
 private const val ROKKU_IMAGE_URL = "${CDN}emojis/1550782013022281728.webp?quality=lossless"
 private const val LIBRARY_IMAGE_URL = "${CDN}emojis/1552645911254138911.webp?quality=lossless"
-private const val updatesImageUrl = "${CDN}emojis/1391945005194674237.webp?quality=lossless"
+// private const val updatesImageUrl = "${CDN}emojis/1391945005194674237.webp?quality=lossless"
 private const val HISTORY_IMAGE_URL = "${CDN}emojis/1552649831149998131.webp?quality=lossless"
 private const val BROWSE_IMAGE_URL = "${CDN}emojis/1552656747083206747.webp?quality=lossless"
 private const val moreImageUrl = "${CDN}emojis/1391947518224371772.webp?quality=lossless"
